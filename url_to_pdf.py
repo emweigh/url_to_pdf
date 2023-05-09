f95856ab3ff2da81dec2dfcaea94ab00019668ca
isalin <isalin@users.noreply.github.com>
2023-05-09 18:03:11 +0200

    Basic script to convert url to pdf.

---
+++ url_to_pdf.py
@@ -0,0 +1,32 @@
+import sys
+import asyncio
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+
+
+def save_pdf(url: str, output_file: str = 'file.pdf'):
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+        print(page.title())
+
+        print("Saving PDF")
+        content = page.content()
+        soup = BeautifulSoup(content, "html.parser")
+        title = soup.title.string if soup.title else "untitled"
+        title = title.replace(" ", "_").replace("/", "_").replace("|", "_")
+        output_path = f"{title}.pdf"
+
+        page.pdf(path=output_path, format="A4")
+        browser.close()
+        print(f"PDF saved as {output_path}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python url_to_pdf.py <url>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    save_pdf(url)
